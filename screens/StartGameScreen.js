b27726e3aa5c4051c89baf75b51cfc52a4ae9f57
meidikawardana <jdeep555.mw@gmail.com>
2020-07-10 14:38:15 +0700

    Color Theming with Constants

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
 import Card from "../components/Card";
+import Colors from "../constants/colors";
 
 const StartGameScreen = (props) => {
   return (
@@ -12,8 +13,16 @@ const StartGameScreen = (props) => {
           <Text>Select a Number</Text>
           <TextInput />
           <View style={styles.buttonContainer}>
-            <Button title="Reset" onPress={() => {}} />
-            <Button title="Confirm" onPress={() => {}} />
+            <View style={styles.button}>
+              <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={() => {}}
+                color={Colors.primary}
+              />
+            </View>
           </View>
         </Card>
       </View>
@@ -32,7 +41,7 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   inputContainer: {
-    width: 300,
+    width: 450,
     maxWidth: "80%",
     alignItems: "center",
   },
@@ -41,6 +50,9 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "space-between",
     paddingHorizontal: 15,
+  },
+  button: {
+    width: 80,
   },
 });
 
