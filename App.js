e1f03d22bef2db4441dcb996c3a10b4c1467f8df
Meidika Wardana <jdeep555.mw@gmail.com>
2020-07-11 16:03:18 +0700

    Finishing the Game Logic

--- App.js
+++ App.js
@@ -3,27 +3,39 @@ import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
-import GameOverScreen from './screens/GameOverScreen';
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
-  const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber);
+  const configureNewGameHandler = () => {
     setGuessRounds(0);
+    setUserNumber(null);
   };
 
-  const gameOverHandler = numOfRounds => {
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  const gameOverHandler = (numOfRounds) => {
     setGuessRounds(numOfRounds);
-  }
+  };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
   if (userNumber && guessRounds <= 0) {
-    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
-  }else if(guessRounds > 0){
-    content = <GameOverScreen />;
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
 
   return (
