a70a0f484c5b31d5f3a464fd9311175a3be667c6
Meidika Wardana <jdeep555.mw@gmail.com>
2020-07-15 12:24:23 +0700

    Building a Custom Button Component

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, Button, Image } from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
 import Colors from "../constants/colors";
+import MainButton from "../components/MainButton";
 
 const GameOverScreen = (props) => {
   return (
@@ -29,7 +30,7 @@ const GameOverScreen = (props) => {
           <Text style={styles.highlight}>{props.userNumber}</Text>.
         </BodyText>
       </View>
-      <Button title="NEW GAME" onPress={props.onRestart} />
+      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
     </View>
   );
 };
