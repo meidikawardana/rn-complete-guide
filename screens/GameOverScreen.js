24d537020908e320af26402f42df3c0f5d8035da
Meidika Wardana <jdeep555.mw@gmail.com>
2020-07-15 11:52:36 +0700

    A Closer Look at the "Text" Component (and what you can do with it)

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, Image } from "react-native";
 
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
 
 const GameOverScreen = (props) => {
   return (
@@ -11,17 +12,23 @@ const GameOverScreen = (props) => {
       <View style={styles.imageContainer}>
         <Image
           fadeDuration={1000} //default is 300ms
-          // source={require("../assets/success.png")}
-          source={{
-            uri:
-              "https://cdn.pixabay.com/photo/2017/08/08/00/12/nature-2609489_960_720.jpg",
-          }}
+          source={require("../assets/success.png")}
+          // source={{
+          //   uri:
+          //     "https://cdn.pixabay.com/photo/2017/08/08/00/12/nature-2609489_960_720.jpg",
+          // }}
           style={styles.image}
           resizeMode="cover" //default is "cover"
         />
       </View>
-      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{" "}
+          <Text style={styles.highlight}>{props.userNumber}</Text>.
+        </BodyText>
+      </View>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -45,6 +52,19 @@ const styles = StyleSheet.create({
   image: {
     width: "100%", //width and height is required for web images
     height: "100%",
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
+  },
+  resultContainer: {
+    // width: "80%",
+    marginHorizontal: 50,
+    marginVertical: 15,
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 20,
   },
 });
 
