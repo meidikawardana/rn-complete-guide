4940a70089e5dc1996f144706a7f2d16375c52a5
Meidika Wardana <jdeep555.mw@gmail.com>
2020-07-17 00:31:48 +0700

    Using Dimensions in "if" Checks

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,8 +4,8 @@ import {
   StyleSheet,
   Text,
   Alert,
-  ScrollView,
   FlatList,
+  Dimensions,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -76,6 +76,10 @@ const GameScreen = (props) => {
     ]);
   };
 
+  let listContainerStyle = styles.listContainer;
+  if (Dimensions.get("window").width < 350)
+    listContainerStyle = styles.listContainerBig;
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
@@ -88,12 +92,7 @@ const GameScreen = (props) => {
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
-        {/* <ScrollView contentContainerStyle={styles.list}>
-          {pastGuesses.map((guess, index) =>
-            renderListItem(guess, pastGuesses.length - index)
-          )}
-        </ScrollView> */}
+      <View style={listContainerStyle}>
         <FlatList
           keyExtractor={(item) => item}
           data={pastGuesses}
@@ -115,7 +114,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 15 : 5,
     width: 400,
     maxWidth: "90%",
     alignItems: "center",
@@ -132,7 +131,11 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     flex: 1,
-    width: "60%",
+    width: "75%",
+  },
+  listContainerBig: {
+    flex: 1,
+    width: "80%",
   },
   list: {
     flexGrow: 1,
