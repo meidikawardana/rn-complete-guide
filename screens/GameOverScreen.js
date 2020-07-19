bc52871818f53791c79bd707778560e47d3ca600
Meidika Wardana <jdeep555.mw@gmail.com>
2020-07-19 10:59:10 +0700

    Fixing the GameOver Screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingTop: 10,
+    paddingVertical: 10,
   },
   imageContainer: {
     borderRadius: (Dimensions.get("window").width * 0.7) / 2,
