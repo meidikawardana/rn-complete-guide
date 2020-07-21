da5b47d5416f6a55282106943c127d5f0a1d227a
Meidika Wardana <jdeep555.mw@gmail.com>
2020-07-21 09:59:44 +0700

    //fix bug: Error: This device does not support the requested orientation 2. https://docs.expo.io/versions/latest/sdk/screen-orientation/#installation

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,6 +6,7 @@ import {
   Alert,
   FlatList,
   Dimensions,
+  Platform,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import * as ScreenOrientation from "expo-screen-orientation";
@@ -35,7 +36,11 @@ const renderListItem = (listLength, itemData) => (
 );
 
 const GameScreen = (props) => {
-  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+  if (Platform.OS === "ios") {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+  } else {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+  }
 
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
