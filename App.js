1ff674e85874a2075e4c8f8b0910d95321911887
Meidika Wardana <jdeep555.mw@gmail.com>
2020-07-22 10:37:50 +0700

    //brand new app. Meals App

--- App.js
+++ App.js
@@ -1,74 +1,10 @@
-import React, { useState } from "react";
-import { StyleSheet, View, SafeAreaView } from "react-native";
-import * as Font from "expo-font";
-import { AppLoading } from "expo";
-
-import Header from "./components/Header";
-import StartGameScreen from "./screens/StartGameScreen";
-import GameScreen from "./screens/GameScreen";
-import GameOverScreen from "./screens/GameOverScreen";
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-  });
-};
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [guessRounds, setGuessRounds] = useState(0);
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  if (!dataLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)}
-        onError={(err) => console.log(err)}
-      />
-    );
-  }
-
-  const configureNewGameHandler = () => {
-    setGuessRounds(0);
-    setUserNumber(null);
-  };
-
-  const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber);
-  };
-
-  const gameOverHandler = (numOfRounds) => {
-    setGuessRounds(numOfRounds);
-  };
-
-  let content = <StartGameScreen onStartGame={startGameHandler} />;
-
-  if (userNumber && guessRounds <= 0) {
-    content = (
-      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-    );
-  } else if (guessRounds > 0) {
-    content = (
-      <GameOverScreen
-        roundsNumber={guessRounds}
-        userNumber={userNumber}
-        onRestart={configureNewGameHandler}
-      />
-    );
-  }
-
   return (
-    <SafeAreaView style={styles.screen}>
-      <Header title="Guess a Number" />
-      {content}
-    </SafeAreaView>
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
-});
--- README.md
+++
@@ -1,2 +0,0 @@
-# rn-complete-guide
-A project for me to learn creating react native app from Maximillian Swarchmuller on Udemy course
