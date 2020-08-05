329b6dd5e3dab98df872fe3f6e14ec13ba045b2d
Meidika Wardana <jdeep555.mw@gmail.com>
2020-08-05 08:03:24 +0700

    25. Fixing the Shadows

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -36,7 +36,9 @@ const styles = StyleSheet.create({
     height: 150,
     margin: 15,
     borderRadius: 10,
-    overflow: "hidden",
+    overflow:
+      Platform.OS == "android" && Platform.Version >= 21 ? "hidden" : "visible",
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -45,7 +47,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
